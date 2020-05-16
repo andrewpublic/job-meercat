f2133e8964cdd18e44e53ae1a904af219b7ca475
unknown <youngminii@gmail.com>
2020-05-16 17:03:36 +1000

    Scaffolding

---
+++ IssueTracker/IssueTracker/Controllers/IssuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IssueTracker.Models;
+
+namespace IssueTracker.Controllers
+{
+    public class IssuesController : Controller
+    {
+        // GET: Issues
+        public ActionResult Random()
+        {
+            var issue = new Issue() { Name = "First Bug" };
+            return View(issue);
+        }
+    }
+}
